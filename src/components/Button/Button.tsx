5c09f1ea9eb2639b6c4f06cd5dc1abc906ca06e1
geraldo_domingos <geraldoneto@locus.software>
2023-02-19 15:09:04 -0300

    ✨feature: add onClick function to Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,9 +2,9 @@ import { IButton } from '../../interfaces/Button'
 
 import style from './style.module.scss'
 
-export default function Button({ title, type }: IButton) {
+export default function Button({ title, type, onClick }: IButton) {
   return (
-    <button className={style.botao} type={type}>
+    <button className={style.botao} type={type} onClick={onClick}>
       {title}
     </button>
   )
